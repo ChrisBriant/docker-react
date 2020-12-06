7d118969f939ed307cb503310f7bffd79a83deea
Chris Briant <58183754+ChrisBriant@users.noreply.github.com>
2020-12-06 09:38:06 +0000

    Change to feature branch

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Fellow Weakling! Welcome to the weak maze.
+          Hello Fellow Weakling! Welcome to the weak maze. I have changed the feature branch.
         </p>
         <a
           className="App-link"
